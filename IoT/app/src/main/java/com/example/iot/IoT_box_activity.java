704b71134e04f8c0bb7292a9fb4eebd120e7f351
saikiran8 <kollojusaikiran@gmail.com>
2021-04-24 15:10:56 +0530

    basic UI added

---
+++ IoT/app/src/main/java/com/example/iot/IoT_box_activity.java
@@ -0,0 +1,14 @@
+package com.example.iot;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class IoT_box_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_io_t_box_activity);
+    }
+}
